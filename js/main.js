992b57664af6421c356bd0fa2aa7d3d64192c207
Ivan-nvkz <66069539+Ivan-nvkz@users.noreply.github.com>
2022-01-05 18:23:05 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//===  main.js  start =========================================================
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+
+});
+//===  main.js  finish =========================================================
+
+
+
+
+
+
+
+
+
+//=====  JQuery  start  =============================================================
+$(function () {
+
+   $('.top__slider').slick({
+      // dots: true,
+      // arrows: false,
+      // fade: true,
+      // autoplay: true
+   });
+
+});
+
+
+
+//=====  JQuery  finish  =============================================================
